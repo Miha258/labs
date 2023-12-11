f69e069f88cc60f69497d38bcaeee6975d81533f
Myhailo Riznyk <myhailo@MBP-Myhailo.lan>
2023-12-12 01:24:16 +0200

    m

--- task8/main.py
+++ task8/main.py
@@ -1,16 +1,19 @@
-def generate_subsets(input_set):
-    # Створюємо пустий список для зберігання всіх підмножин
-    all_subsets = []
-    # Отримуємо кількість елементів у вхідній множині
-    n = len(input_set)
+from itertools import combinations
 
-    # Зовнішній цикл пройде 2^n разів, де n - кількість елементів
-    for i in range(2**n):
-        # Перетворюємо число i у його двійкове представлення
-        subset = [input_set[j] for j in range(n) if (i // (2**j)) % 2 == 1]
-        
-        # Додаємо підмножину до списку всіх підмножин
-        all_subsets.append(subset)
-    
-    # Повертаємо список всіх підмножин
+def generate_subsets(input_set):
+    all_subsets = []
+    # Цикл пройде від 0 до n, визначаючи розмір підмножини
+    for r in range(len(input_set) + 1):
+        # Використовуємо combinations для отримання всіх комбінацій розміром r
+        subsets_of_size_r = combinations(input_set, r)
+        # Додаємо отримані підмножини до списку всіх підмножин
+        all_subsets.extend(subsets_of_size_r)
     return all_subsets
+
+# Вхідна множина
+input_set = {1, 2, 3}
+subsets = generate_subsets(input_set)
+
+# Виводимо кожну підмножину на екран
+for subset in subsets:
+    print(set(subset))
